a7328436b83cd5bae5d7e8fd73c0708370da1ad4
0x000011b <0x000011b@proton.me>
2023-02-06 16:22:55 -0300

    chore: properly document what the Account component is supposed to be

--- src/pages/Account/index.tsx
+++ src/pages/Account/index.tsx
@@ -1,8 +1,6 @@
-import Login from "./Login";
-
-const Account = () => 
-  // TODO: Account Page
-   <Login />
-;
-
+const Account = () => (
+  // TODO(11b): This should be the account page where the user can see and edit
+  // his own info (password, display name, created bots, etc.).
+  <p>not implemented yet</p>
+);
 export default Account;
