76952c63dccc34e778ccdaea74f475f1db64370a
0x000011b <0x000011b@proton.me>
2023-02-09 11:06:03 -0300

    feat: improvements to the `TextInput` component

--- src/shared/TextInput.tsx
+++ src/shared/TextInput.tsx
@@ -1,25 +1,27 @@
 import { Component, Show, createMemo } from "solid-js";
 
 const TextInput: Component<{
-  label?: string;
   fieldName: string;
+  label?: string;
   helperText?: string;
   placeholder?: string;
   isMultiline?: boolean;
-  class?: string;
   type?: string;
+  required?: boolean;
 }> = (props) => {
-  const placeholder = createMemo(
-    () => props.placeholder || "Type something here..."
+  const placeholder = createMemo(() =>
+    props.placeholder !== undefined
+      ? props.placeholder
+      : "Type something here..."
   );
 
   return (
-    <div class={props.class}>
+    <div>
       <Show when={!!props.label}>
         <label for={props.fieldName}>
-          {props.label}
+          <div class={props.helperText ? "" : "pb-1"}>{props.label}</div>
           <Show when={!!props.helperText}>
-            <p class="mt-[-0.125rem] pb-2 text-sm text-white/50">
+            <p class="mt-[-0.125rem] pb-1 text-sm text-white/50">
               {props.helperText}
             </p>
           </Show>
@@ -32,6 +34,7 @@ const TextInput: Component<{
             id={props.fieldName}
             name={props.fieldName}
             type={props.type || "text"}
+            required={props.required}
             placeholder={placeholder()}
             class="focusable-field w-full rounded-xl px-4 py-2"
           />
@@ -42,12 +45,13 @@ const TextInput: Component<{
           // https://stackoverflow.com/questions/454202/creating-a-textarea-with-auto-resize?noredirect=1
           id={props.fieldName}
           name={props.fieldName}
+          required={props.required}
           placeholder={placeholder()}
           class="focusable-field w-full rounded-xl px-4 py-2 !text-white"
           onInput={(e) => {
             const ele = e.target as HTMLTextAreaElement;
             ele.style.height = "";
-            ele.style.height = `${ele.scrollHeight  }px`;
+            ele.style.height = `${ele.scrollHeight}px`;
           }}
         />
       </Show>
