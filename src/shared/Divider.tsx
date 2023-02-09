2b1a1de029e2daa4a1df64fcb1bbfb8257d948b9
0x000011b <0x000011b@proton.me>
2023-02-09 12:03:23 -0300

    feat: reusable `Divider` component

---
+++ src/shared/Divider.tsx
@@ -0,0 +1,6 @@
+import { Component } from "solid-js";
+
+/** A subtle horizontal divider line. */
+const Divider: Component = () => <div class="my-4 border-b border-white/5" />;
+
+export default Divider;
--- src/shared/PageHeader.tsx
+++ src/shared/PageHeader.tsx
@@ -1,12 +1,14 @@
 import { Component, Show } from "solid-js";
 
+import Divider from "./Divider";
+
 const PageHeader: Component<{ title: string; subtitle?: string }> = (props) => (
   <>
     <h1 class="text-4xl">{props.title}</h1>
     <Show when={!!props.subtitle}>
       <p class="text-white/50">{props.subtitle}</p>
     </Show>
-    <div class="my-4 border-b border-white/5" />
+    <Divider />
   </>
 );
 
