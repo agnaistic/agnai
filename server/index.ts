f5fb30bb46e23c9500dab90318bbd0f702fb6dce
Sceuick <leikho@gmail.com>
2023-02-13 21:43:00 +0800

    Add db folder

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
 import { config } from './config'
-import { store } from './db'
+import { db, store } from './db'
 import { logger } from './logger'
 
 app.listen(config.port, '0.0.0.0', async () => {
   logger.info({ port: config.port }, 'Server started')
+  const info = await db().info()
   const settings = await store.settings.get()
-  logger.info({ settings }, 'Database info')
+  logger.info({ info, settings }, 'Database info')
 })
