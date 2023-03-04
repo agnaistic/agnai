4b2bba2b9aabcd54e1a23eb287690b9e907a70c1
Sceuick <leikho@gmail.com>
2023-03-04 09:56:14 +0800

    Rename ci file

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-STACK=ci.docker-compose.yml
+STACK=agn.docker-compose.yml
 
-echo "Skipping deployment"
-
-# scp scripts/$STACK ci@$@:/home/ci/$STACK
+scp .github/$STACK ci@$@:/home/ci/$STACK
 # ssh ci@$@ "docker stack deploy -c /home/ci/$STACK agnaistic"
