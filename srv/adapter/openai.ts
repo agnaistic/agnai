2b63a3e9bec6ac988766f9efc3da6dd55941ecc4
ewof <elwolf6@protonmail.com>
2023-03-11 23:26:19 -0500

    fixes using OpenAI when in guest mode (#32)

--- srv/adapter/openai.ts
+++ srv/adapter/openai.ts
@@ -24,7 +24,6 @@ export const handleOAI: ModelAdapter = async function* ({
     yield { error: `OpenAI request failed: Not configured` }
     return
   }
-
   const oaiModel = settings.oaiModel ?? defaultPresets.openai.oaiModel
 
   const body: any = {
@@ -67,9 +66,11 @@ export const handleOAI: ModelAdapter = async function* ({
     body.prompt = prompt
   }
 
+  const bearer = user._id === "anon" ? `Bearer ${user.oaiKey}` : `Bearer ${decryptText(user.oaiKey)}`
+
   const headers = {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${decryptText(user.oaiKey)}`,
+    Authorization: bearer,
   }
 
   const url = turbo ? `${baseUrl}/chat/completions` : `${baseUrl}/completions`
