56e1f72abff6efd93d391e2076cb8aaa9bbadc02
Sceuick <leikho@gmail.com>
2023-03-13 08:34:53 +0800

    Delete chats and messages when deleting character

--- srv/db/characters.ts
+++ srv/db/characters.ts
@@ -51,6 +51,9 @@ export async function getCharacters(userId: string) {
 
 export async function deleteCharacter(opts: { charId: string; userId: string }) {
   await db('character').deleteOne({ _id: opts.charId, userId: opts.userId, kind: 'character' }, {})
+  const chats = await db('chat').find({ characterId: opts.charId, userId: opts.userId }).toArray()
+  await db('chat-message').deleteMany({ chatId: { $in: chats.map((ch) => ch._id) } })
+  await db('chat').deleteMany({ characterId: opts.charId, userId: opts.userId })
 }
 
 export async function getCharacterList(charIds: string[]) {
