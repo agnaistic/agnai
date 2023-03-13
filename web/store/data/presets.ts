bd2fb88f570be1b6517a6927b296087c358792cb
Sceuick <leikho@gmail.com>
2023-03-13 11:47:42 +0800

    Fix type error in local preset delete

--- web/store/data/presets.ts
+++ web/store/data/presets.ts
@@ -2,7 +2,6 @@ import { v4 } from 'uuid'
 import { AppSchema } from '../../../srv/db/schema'
 import { api, isLoggedIn } from '../api'
 import { loadItem, local } from './storage'
-import { emptyPreset } from '../../pages/GenerationPresets'
 
 export type PresetUpdate = Omit<AppSchema.UserGenPreset, '_id' | 'kind' | 'userId'>
 
@@ -57,26 +56,7 @@ export async function deletePreset(presetId: string) {
   }
 
   const presets = loadItem('presets')
-  let preset: AppSchema.UserGenPreset = { _id: '', ...emptyPreset, kind: 'gen-setting', userId: '' }
-
-  for (let i = 0; i < presets.length; i++) {
-    const element = presets[i]
-    if (element._id === presetId) {
-      preset = element
-    }
-  }
-
-  if (preset._id !== '') {
-    local.savePresets(presets.splice(presets.indexOf(preset, 1)))
-    return {
-      result: -1,
-      status: 500,
-      error: 'Preset not found in Local Storage',
-    }
-  }
-
-  return {
-    result: 0,
-    status: 200,
-  }
+  const next = presets.filter((pre) => pre._id !== presetId)
+  local.savePresets(next)
+  return local.result({ success: true })
 }
