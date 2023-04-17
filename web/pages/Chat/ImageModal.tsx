50ec581bd1f927fa9d1212124bbaa2f2ba5dd794
Sceuick <leikho@gmail.com>
2023-04-17 09:55:14 +0800

    Fix gen image in image modal

--- web/pages/Chat/ImageModal.tsx
+++ web/pages/Chat/ImageModal.tsx
@@ -1,6 +1,7 @@
 import { Component } from 'solid-js'
 import { NoTitleModal } from '../../shared/Modal'
 import { msgStore } from '../../store'
+import { getAssetUrl } from '../../shared/util'
 
 export const ImageModal: Component = () => {
   const state = msgStore()
@@ -8,7 +9,7 @@ export const ImageModal: Component = () => {
   return (
     <NoTitleModal show={!!state.showImage} close={() => msgStore.showImage()} maxWidth="half">
       <div class="flex justify-center p-4">
-        <img class="rounded-md" src={state.showImage!.msg} />
+        <img class="rounded-md" src={getAssetUrl(state.showImage!.msg)} />
       </div>
     </NoTitleModal>
   )
