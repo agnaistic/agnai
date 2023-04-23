073189f8a440f5e4a368a2a63b37ffe9ee5304a0
LT <lt455067@gmail.com>
2023-04-22 23:00:10 -0400

    feat: adding interactivity to policy gate page

--- web/pages/PolicyGate/index.tsx
+++ web/pages/PolicyGate/index.tsx
@@ -7,10 +7,14 @@ import NavBar from "../../shared/NavBar";
 import Navigation from "../../Navigation";
 import Toasts from "../../Toasts";
 import Modal from "../../shared/Modal";
+import TermsOfService from "../TermsOfService";
+import PrivacyPolicy from "../PrivacyPolicy";
+import Button from "../../shared/Button";
 
 const PolicyGate: Component = () => {
-  const [showTermsOfService, setShowTermsOfService] = createSignal('')
-  const [showPrivacyPolicy, setShowPrivacyPolicy] = createSignal('')
+  const [showTermsOfService, setShowTermsOfService] = createSignal(false)
+  const [showPrivacyPolicy, setShowPrivacyPolicy] = createSignal(false)
+  const [isPolicyAccepted, setIsPolicyAccepted] = createSignal(false)
   return (
     <div>
       <PageHeader
@@ -22,22 +26,22 @@ const PolicyGate: Component = () => {
       />
       <div class="flex flex-col gap-4">
         <p>Before you can proceed, you must verify your age and agree to our Terms of Service and Privacy Policy.</p>
-        <div class="max-w-[20rem] min-w-[3rem] flex flex-row justify-around">
-          <div>Terms of Service</div>
-          <div>Privacy Policy</div>
+        <div class="max-w-[30rem] min-w-[3rem] flex flex-row justify-around">
+          <Button onClick={() => setShowTermsOfService(true)}>Read Terms of Service</Button>
+          <Button onClick={() => setShowPrivacyPolicy(true)}>Read Privacy Policy</Button>
         </div>
-        <Modal close={() => setShowTermsOfService('')} show={!!showTermsOfService()}>
+        <Modal maxWidth="full" close={() => setShowTermsOfService(false)} show={showTermsOfService()}>
           <TermsOfService/>
         </Modal>
-        <Modal close={() => setShowPrivacyPolicy('')} show={!!showPrivacyPolicy()}>
+        <Modal maxWidth="full" close={() => setShowPrivacyPolicy(false)} show={showPrivacyPolicy()}>
           <PrivacyPolicy/>
         </Modal>
         <div>
-          <label><input class="inline-block mx-2" type="checkbox" id="policy-check" />Check here to agree to our Terms of Service and Privacy Policy</label>
+          <label><input class="inline-block mx-2" type="checkbox" id="policy-check" onChange={(event) => setIsPolicyAccepted(event.currentTarget.checked)} />Check here to agree to our Terms of Service and Privacy Policy</label>
         </div>
-        <div class="flex-col justify-around">
-          <div>18 years old or older? Click Here!</div>
-          <div>Not 18? Click Here!</div>
+        <div class="max-w-[30rem] min-w-[3rem] flex flex-row justify-around">
+          <Button disabled={!isPolicyAccepted()}>18 years old or older? Click Here!</Button>
+          <Button disabled={!isPolicyAccepted()}>Not 18? Click Here!</Button>
         </div>
       </div>
     </div>
