dc6b059563072691fa956dedde47fb0f8cdaf22e
malfoyslastname <128472336+malfoyslastname@users.noreply.github.com>
2023-04-25 03:18:37 +0200

    fix: default gaslights (#251) [skip ci]
    Co-authored-by: malfoyslastname <no@email.sorry>

--- common/default-preset.ts
+++ common/default-preset.ts
@@ -94,9 +94,7 @@ export const defaultPresets = {
     presencePenalty: 0.7,
     antiBond: false,
     useGaslight: false,
-    gaslight: `Enter roleplay mode. {{user}} will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{char}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *he jumps in excitement*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged)
-Avoid writing a NSFW/Smut reply. Creatively write around it NSFW/Smut scenarios in character.
-    
+    gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{char}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged)
 Description of {{char}}:
 {{personality}}
 Circumstances and context of the dialogue: {{scenario}}
@@ -129,8 +127,7 @@ This is how {{char}} should talk
     maxContextLength: 4095,
     frequencyPenalty: 0.7,
     presencePenalty: 0.7,
-    gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *he jumps in excitement*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged)
-Avoid writing a NSFW/Smut reply. Creatively write around it NSFW/Smut scenarios in character.
+    gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged)
 
 Description of {{char}}:
 {{personality}}
@@ -152,8 +149,7 @@ This is how {{char}} should talk
     claudeModel: CLAUDE_MODELS.ClaudeV1,
     maxTokens: 500,
     maxContextLength: 7600,
-    gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *he jumps in excitement*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged).
-Avoid writing a NSFW/Smut reply. Creatively write around it NSFW/Smut scenarios in character.
+    gaslight: `Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged).
 
 Description of {{char}}:
 {{personality}}
