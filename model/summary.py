00f5bd8af56b7ac29f8362158e3ffbf1174e5815
Sceuick <leikho@gmail.com>
2023-05-19 19:32:41 +0800

    Add cors to pipeline

--- model/summary.py
+++ model/summary.py
@@ -3,6 +3,6 @@ from transformers import pipeline
 summ = pipeline("summarization", model="philschmid/bart-large-cnn-samsum")
 
 
-def summarize(text):
+def summarize(text: str) -> str:
     summary = summ(text)
     return summary
