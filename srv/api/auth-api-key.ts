13f6743cba64b65f687de398e142624120881240
Acid Bubbles <acidbubbles.anon@gmail.com>
2023-05-21 22:29:28 -0400

    Add user to API-authenticated requests

--- srv/api/auth-api-key.ts
+++ srv/api/auth-api-key.ts
@@ -1,5 +1,6 @@
 import { Response, NextFunction } from 'express'
 import { store } from '../db'
+import { AppSchema } from '../db/schema'
 import { AppRequest } from './wrap'
 
 export const authApiKeyMiddleware: any = async (
@@ -16,13 +17,21 @@ export const authApiKeyMiddleware: any = async (
     return next()
   }
 
+  // TODO: Keep track in Redis and/or local cache if the API is used a lot
   const keyRecord = await store.apiKey.getApiKeyByKey(apiKey)
 
   if (!keyRecord) {
     return res.status(401).send({ error: 'Invalid API key' })
   }
 
-  // Attaching user id to the request for further use in the route handler.
   req.userId = keyRecord.userId
+  req.user = {
+    userId: keyRecord.userId,
+    username: 'api user',
+    admin: false,
+    exp: 0,
+    iat: 0,
+  } as AppSchema.Token
+
   next()
 }
