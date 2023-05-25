0d42133270ba0782801b4296c940555a40d498b6
Sceuick <leikho@gmail.com>
2023-05-25 11:40:34 +0800

    Tweak image summary helper text

--- web/pages/Settings/Image/ImageSettings.tsx
+++ web/pages/Settings/Image/ImageSettings.tsx
@@ -82,7 +82,7 @@ export const ImageSettings: Component = () => {
       <Toggle
         fieldName="summariseChat"
         label="Summarise Chat"
-        helperText="EXPERIMENTAL: When available use your AI service to summarise the chat into an image prompt. Currently only available with OpenAI."
+        helperText="EXPERIMENTAL: When available use your AI service to summarise the chat into an image prompt. Currently only available with OpenAI and NovelAI."
         value={state.user?.images?.summariseChat}
       />
 
