2fed4ed9306248211ea53adb6f106d746356d67b
replikahater <replikahater525@gmail.com>
2023-06-24 11:28:48 +0200

    Proposed tweaks to default presets
    - Fix duplicated closing tag in Claude preset
    - Move "Do not decide what {{user}} says or does" from Claude system prompt to UJB
    - Raise temperature from 0.5 to 0.8 (Experiments show temperature higher than 0.5 is always good)
    - Reduce Frequency/Presence penalty from 0.7 to 0.5 (Experiments show
      higher penalties make long conversations change personality with time)
    - OpenAI Top P should be 1 by default (they recommend changing eith)

--- common/presets/claude.ts
+++ common/presets/claude.ts
@@ -16,10 +16,9 @@ export const claudePresets = {
     ignoreCharacterSystemPrompt: false,
     ignoreCharacterUjb: false,
     systemPrompt: `<writing_task>
-Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Do not decide what {{user}} says or does. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged).
+Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between {{char}} and {{user}}. Use Internet roleplay style, e.g. no quotation marks, and write user actions in italic in third person like: *example*. You are allowed to use markdown. Be proactive, creative, drive the plot and conversation forward. Write at least one paragraph, up to four. Always stay in character. Always keep the conversation going. (Repetition is highly discouraged).
 </writing_task>`,
     gaslight: `{{system_prompt}}
-</writing_task>
 <character_description>
 {{personality}}
 </character_description>
@@ -29,5 +28,6 @@ Enter roleplay mode. You will write {{char}}'s next reply in a dialogue between 
 <facts>
 {{memory}}
 </facts>`,
+  ultimeJailbreak: `Do not decide what {{user}} says or does.`
   },
 } satisfies Record<string, Partial<AppSchema.GenSettings>>
