ef602b57b2930c1b7e927e377a348370cc0e9dbd
Sceuick <leikho@gmail.com>
2023-06-24 13:28:18 +0800

    message markdown tweaks

--- web/pages/Chat/components/InputBar.tsx
+++ web/pages/Chat/components/InputBar.tsx
@@ -152,7 +152,7 @@ const InputBar: Component<{
         value={text()}
         placeholder={placeholder()}
         parentClass="flex w-full"
-        class="h-full rounded-md rounded-r-none hover:bg-[var(--bg-800)] active:bg-[var(--bg-800)]"
+        class="input-bar h-full rounded-r-none hover:bg-[var(--bg-800)] active:bg-[var(--bg-800)]"
         onKeyDown={(ev) => {
           if (ev.key === 'Enter' && !ev.shiftKey) {
             send()
