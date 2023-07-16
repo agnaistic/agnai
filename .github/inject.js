b5839c7793a99b2056ad654b805f56edd0887416
Sceik <60520115+sceuick@users.noreply.github.com>
2023-07-16 13:32:12 +0800

    Fix/image version (#510)

--- .github/inject.js
+++ .github/inject.js
@@ -4,18 +4,18 @@ const fs = require('fs')
 const inject = process.env.INJECT_SCRIPT
 console.log('Injecting:', !!inject)
 
+const tags = ['<meta inject="">', '<meta inject>', '<meta inject="" />']
+const indexFile = path.resolve(__dirname, '../dist/index.html')
+const outFile = path.resolve(__dirname, '../dist/index.html')
+
+let content = fs.readFileSync(indexFile).toString().replace('{{unknown}}', process.env.GITHUB_SHA)
+
 if (inject) {
-  const tags = ['<meta inject="">', '<meta inject>', '<meta inject="" />']
-  const indexFile = path.resolve(__dirname, '../dist/index.html')
-  const outFile = path.resolve(__dirname, '../dist/index.html')
-
-  let content = fs.readFileSync(indexFile).toString().replace('{{unknown}}', process.env.GITHUB_SHA)
-
   for (const tag of tags) {
     if (!content.includes(tag)) continue
     content = content.replace(tag, inject)
     break
   }
-
-  fs.writeFileSync(outFile, content)
 }
+
+fs.writeFileSync(outFile, content)
