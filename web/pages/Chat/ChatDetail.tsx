cfbf8a394c4407b069cdba802ab74af1f30f8b46
Sceuick <leikho@gmail.com>
2023-07-22 18:42:16 +0800

    Remove log

--- web/pages/Chat/ChatDetail.tsx
+++ web/pages/Chat/ChatDetail.tsx
@@ -248,7 +248,6 @@ const ChatDetail: Component = () => {
   }
 
   const togglePane = (paneType: ChatRightPane) => {
-    console.log('Setpane', paneType)
     setShowOpts(false)
     chatStore.option('pane', chats.opts.pane === paneType ? undefined : paneType)
     setSearch({ pane: paneType })
