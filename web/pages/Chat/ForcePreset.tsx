6303f740e1fc1ad55f461dd413d525bfdf0776e2
Sceuick <leikho@gmail.com>
2023-09-08 23:04:35 +0800

    Remove unused refs

--- web/pages/Chat/ForcePreset.tsx
+++ web/pages/Chat/ForcePreset.tsx
@@ -1,17 +1,14 @@
 import { Component, createMemo, createSignal, onMount } from 'solid-js'
 import { AppSchema } from '../../../common/types/schema'
 import { AutoPreset, getPresetOptions } from '../../shared/adapter'
-import { chatStore, presetStore, settingStore, toastStore } from '../../store'
-import { ADAPTER_LABELS } from '../../../common/adapters'
-import { defaultPresets, isDefaultPreset } from '../../../common/presets'
+import { chatStore, presetStore, toastStore } from '../../store'
+import { isDefaultPreset } from '../../../common/presets'
 import { isEligible } from './util'
 
 const ForcePresetModal: Component<{ chat: AppSchema.Chat; show: boolean; close: () => void }> = (
   props
 ) => {
-  let ref: any
   const presets = presetStore((s) => s.presets)
-  const adapters = settingStore((s) => s.config.adapters)
 
   const options = createMemo(() => {
     const opts = getPresetOptions(presets, { builtin: true }).filter((pre) => pre.value !== 'chat')
@@ -19,14 +16,8 @@ const ForcePresetModal: Component<{ chat: AppSchema.Chat; show: boolean; close: 
   })
 
   const [presetId, setPresetId] = createSignal(props.chat.genPreset || options()[0].value)
-  const [preset, setPreset] = createSignal<AppSchema.UserGenPreset>()
-  const [service, setService] = createSignal<string>()
-  const [actual, setActual] = createSignal<AppSchema.GenSettings>()
-
-  const services = createMemo(() => {
-    const list = adapters.map((adp) => ({ value: adp, label: ADAPTER_LABELS[adp] }))
-    return [{ label: 'None', value: '' }].concat(list)
-  })
+  const [preset] = createSignal<AppSchema.UserGenPreset>()
+  const [service] = createSignal<string>()
 
   const savePreset = () => {
     const id = presetId()
@@ -44,21 +35,6 @@ const ForcePresetModal: Component<{ chat: AppSchema.Chat; show: boolean; close: 
 
       presetStore.updatePreset(userPreset._id, { service: svc as any })
     }
-  }
-
-  const onPresetChange = (id: string) => {
-    setPresetId(id)
-
-    const userPreset = presets.find((p) => p._id === id)
-    const actualPreset = userPreset
-      ? userPreset
-      : isDefaultPreset(id)
-      ? defaultPresets[id]
-      : undefined
-
-    setActual(actualPreset as any)
-    setService(userPreset?.service || '')
-    setPreset(userPreset as any)
   }
 
   onMount(() => {
