7d7d3df7d3c382578c82afb72aa46dfd10750bc0
Sceuick <leikho@gmail.com>
2023-09-17 10:40:38 +0800

    Always coerce to number

--- web/pages/Memory/Memory.tsx
+++ web/pages/Memory/Memory.tsx
@@ -197,7 +197,7 @@ function validateBookJson(json: any) {
     }
   }
 
-  book.entries = entries
+  json.entries = entries
 
   assertValid(
     {
@@ -220,7 +220,6 @@ function validateBookJson(json: any) {
 }
 
 function toNumber(value: any) {
-  if (!isNaN(value)) return value
   const num = +value
 
   if (isNaN(num)) return 0
