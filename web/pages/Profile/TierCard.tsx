134c2f23368d2fa68ec7b56099c0a6f5db1b032f
Sceuick <leikho@gmail.com>
2023-10-08 14:33:02 +0800

    Better tier preview

--- web/pages/Profile/TierCard.tsx
+++ web/pages/Profile/TierCard.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'solid-js'
+import { Component, createMemo } from 'solid-js'
 import { AppSchema } from '/common/types'
 import { TitleCard } from '/web/shared/Card'
 import { markdown } from '/web/shared/markdown'
@@ -11,15 +11,17 @@ type TierPreview = OmitId<
 export const TierCard: Component<{ tier: TierPreview; children?: any; class?: string }> = (
   props
 ) => {
+  const cost = createMemo(() => {
+    if (props.tier.cost > 0) return '$' + (props.tier.cost / 100).toFixed(2) + '/mo'
+    return 'None'
+  })
   return (
     <TitleCard class={`flex w-1/2 flex-col gap-2 ${props.class || ''}`}>
       <div class="flex justify-center text-lg font-bold text-[var(--hl-500)]">
         {props.tier.name}
       </div>
       <div class="markdown text-sm" innerHTML={markdown.makeHtml(props.tier.description)} />
-      <div class="flex justify-center text-lg font-bold">
-        ${(props.tier.cost / 100).toFixed(2)}/mo
-      </div>
+      <div class="flex justify-center text-lg font-bold">{cost()}</div>
       {props.children}
     </TitleCard>
   )
