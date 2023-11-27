9577483734907114b5dcdc3a82d78ff8e8d8dc4c
Sceuick <leikho@gmail.com>
2023-11-27 23:39:09 +0800

    Add patreon price

--- web/pages/Profile/TierCard.tsx
+++ web/pages/Profile/TierCard.tsx
@@ -1,4 +1,4 @@
-import { Component, createMemo } from 'solid-js'
+import { Component, Show, createMemo } from 'solid-js'
 import { AppSchema } from '/common/types'
 import { SolidCard } from '/web/shared/Card'
 import { markdown } from '/web/shared/markdown'
@@ -11,17 +11,37 @@ type TierPreview = OmitId<
 export const TierCard: Component<{ tier: TierPreview; children?: any; class?: string }> = (
   props
 ) => {
-  const cost = createMemo(() => {
-    const prices: string[] = []
+  const stripeCost = createMemo(() => {
+    const prices: any[] = []
     if (props.tier.cost > 0) {
-      prices.push('$' + (props.tier.cost / 100).toFixed(2) + '/mo')
+      const cost = (
+        <div>
+          ${(props.tier.cost / 100).toFixed(2)}/mo <span class="text-600 text-xs">Stripe</span>
+        </div>
+      )
+      return cost
     }
 
-    if (prices.length > 0) {
-      return prices.join(' or ')
+    if (props.tier.patreon?.cost) {
+      const cost = (props.tier.patreon?.cost / 100).toFixed(2)
+      prices.push(`Patreon: $${cost}/mo`)
     }
 
-    return 'None'
+    return null
+  })
+
+  const patreonCost = createMemo(() => {
+    if (props.tier.patreon?.cost) {
+      const cost = (
+        <div>
+          ${(props.tier.patreon.cost / 100).toFixed(2)}/mo{' '}
+          <span class="text-600 text-xs">Patreon</span>
+        </div>
+      )
+      return cost
+    }
+
+    return null
   })
 
   return (
@@ -36,7 +56,13 @@ export const TierCard: Component<{ tier: TierPreview; children?: any; class?: st
         <div class="markdown text-sm" innerHTML={markdown.makeHtml(props.tier.description)} />
       </div>
       <div>
-        <div class="flex justify-center text-lg font-bold">{cost()}</div>
+        <div class="text-md flex flex-col items-center font-bold">
+          {stripeCost()}
+          <Show when={props.tier.cost > 0 && !!props.tier.patreon?.cost}>
+            <div class="text-xs">or</div>
+          </Show>
+          {patreonCost()}
+        </div>
         {props.children}
       </div>
     </SolidCard>
