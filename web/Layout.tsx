0495e1f510244d2eae75e01b6e3a974aa6ea0aad
Sceuick <dev@agnai.chat>
2024-04-08 09:29:58 +0800

    Tweak header

--- web/Layout.tsx
+++ web/Layout.tsx
@@ -1,6 +1,6 @@
 import { Component } from 'solid-js'
 import { Portal } from 'solid-js/web'
 
-export const SiteHeader: Component<{ children: any }> = (props) => {
+export const PageHeader: Component<{ children: any }> = (props) => {
   return <Portal mount={document.getElementById('site-header')!}>{props.children}</Portal>
 }
