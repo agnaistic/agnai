567e26c5c271b770bead0735836b9bc57f6d60e5
Sceuick <dev@agnai.chat>
2024-09-05 12:12:18 +0800

    fix type error

--- web/shared/CustomSelect.tsx
+++ web/shared/CustomSelect.tsx
@@ -1,10 +1,17 @@
-import { Component, JSX } from "solid-js"
+import { Component, JSX } from 'solid-js'
 
 export type CustomOption = {
   label: string | JSX.Element
   value: any
 }
 
-export const CustomSelect: Component<{ options:  }> = props => {
+export const CustomSelect: Component<{
+  label?: string
+  helperText?: string
+  fieldName?: string
+  options: CustomOption[]
+  selected?: any
+  onSelect: (opt: CustomOption) => void
+}> = (props) => {
   return null
 }
