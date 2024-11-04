5647799810cd24905de8cb1142a3f429a8c1c82f
Sceuick <dev@agnai.chat>
2024-11-04 13:48:57 +0800

    fix character book create/updates

--- web/pages/Character/form/MemoryBookPicker.tsx
+++ web/pages/Character/form/MemoryBookPicker.tsx
@@ -1,11 +1,10 @@
 import { Save } from 'lucide-solid'
-import { Component, createSignal, createMemo, Show } from 'solid-js'
+import { Component, createSignal, createMemo, Show, createEffect, on } from 'solid-js'
 import EditMemoryForm, { EntrySort } from '../../Memory/EditMemory'
-import { BUNDLED_CHARACTER_BOOK_ID, emptyBookWithEmptyEntry } from '/common/memory'
+import { BUNDLED_CHARACTER_BOOK_ID, emptyBook, emptyBookWithEmptyEntry } from '/common/memory'
 import { AppSchema } from '/common/types'
 import Button from '/web/shared/Button'
-import { useRootModal } from '/web/shared/hooks'
-import Modal from '/web/shared/Modal'
+import { RootModal } from '/web/shared/Modal'
 import Select, { Option } from '/web/shared/Select'
 import { memoryStore } from '/web/store'
 import { createStore } from 'solid-js/store'
@@ -25,21 +24,38 @@ export const MemoryBookPicker: Component<{
 
   const [state, setState] = createStore<AppSchema.MemoryBook>(emptyBookWithEmptyEntry())
 
+  createEffect(
+    on(
+      () => props.bundledBook,
+      (book) => {
+        if (!book) {
+          setState(emptyBook())
+        } else {
+          setState(book)
+        }
+      }
+    )
+  )
+
   const NONE_VALUE = '__none_character_book__'
   const internalMemoryBookOptions = createMemo(() => [
     { label: 'Import Memory Book', value: NONE_VALUE },
     ...memory.books.list.map((book) => ({ label: book.name, value: book._id })),
   ])
+
   const pickInternalMemoryBook = (option: Option) => {
     const newBook = memory.books.list.find((book) => book._id === option.value)
     props.setBundledBook(newBook ? { ...newBook, _id: BUNDLED_CHARACTER_BOOK_ID } : undefined)
   }
+
   const initBlankCharacterBook = () => {
     props.setBundledBook(emptyBookWithEmptyEntry())
   }
+
   const deleteBook = () => {
     props.setBundledBook(undefined)
   }
+
   const ModalFooter = () => (
     <>
       <Button schema="secondary" onClick={() => setIsModalShown(false)}>
@@ -58,56 +74,53 @@ export const MemoryBookPicker: Component<{
     setIsModalShown(false)
   }
 
-  const BookModal = (
-    <Modal
-      title="Character Memory"
-      show={isModalShown()}
-      close={() => setIsModalShown(false)}
-      footer={<ModalFooter />}
-      onSubmit={onSubmitCharacterBookChanges}
-      maxWidth="half"
-      fixedHeight
-    >
-      <div class="text-sm">
-        <EditMemoryForm
-          hideSave
-          state={props.bundledBook!}
-          entrySort={entrySort()}
-          updateEntrySort={updateEntrySort}
-          setter={setState}
-        />
-      </div>
-    </Modal>
-  )
-
-  useRootModal({ id: 'memoryBook', element: BookModal })
-
   return (
-    <div>
-      <h4 class="flex gap-1 text-lg">
-        <div>Character Book</div>
-        <Button size="sm" onClick={initBlankCharacterBook}>
-          Create New Book
-        </Button>
-      </h4>
-      <Show when={!props.bundledBook}>
-        <span class="text-sm"> This character doesn't have a Character Book. </span>
-        <div class="flex flex-col gap-3 sm:flex-row">
-          <Select
-            fieldName="memoryBook"
-            value={NONE_VALUE}
-            items={internalMemoryBookOptions()}
-            onChange={pickInternalMemoryBook}
+    <>
+      <div>
+        <h4 class="flex gap-1 text-lg">
+          <div>Character Book</div>
+          <Button size="sm" onClick={initBlankCharacterBook}>
+            Create New Book
+          </Button>
+        </h4>
+        <Show when={!props.bundledBook}>
+          <span class="text-sm"> This character doesn't have a Character Book. </span>
+          <div class="flex flex-col gap-3 sm:flex-row">
+            <Select
+              fieldName="memoryBook"
+              value={NONE_VALUE}
+              items={internalMemoryBookOptions()}
+              onChange={pickInternalMemoryBook}
+            />
+          </div>
+        </Show>
+        <Show when={props.bundledBook}>
+          <span class="text-sm">This character has a Character Book.</span>
+          <div class="mt-2 flex gap-3">
+            <Button onClick={() => setIsModalShown(true)}>Edit Book</Button>
+            <Button onClick={deleteBook}>Delete Book</Button>
+          </div>
+        </Show>
+      </div>
+      <RootModal
+        title="Character Memory"
+        show={isModalShown()}
+        close={() => setIsModalShown(false)}
+        footer={<ModalFooter />}
+        onSubmit={onSubmitCharacterBookChanges}
+        maxWidth="half"
+        fixedHeight
+      >
+        <div class="text-sm">
+          <EditMemoryForm
+            hideSave
+            state={state}
+            entrySort={entrySort()}
+            updateEntrySort={updateEntrySort}
+            setter={setState}
           />
         </div>
-      </Show>
-      <Show when={props.bundledBook}>
-        <span class="text-sm">This character has a Character Book.</span>
-        <div class="mt-2 flex gap-3">
-          <Button onClick={() => setIsModalShown(true)}>Edit Book</Button>
-          <Button onClick={deleteBook}>Delete Book</Button>
-        </div>
-      </Show>
-    </div>
+      </RootModal>
+    </>
   )
 }
