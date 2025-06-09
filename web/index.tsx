0f10575a99350764becbc6860692abf3c8ee5a27
agnaidev <dev@agnai.chat>
2025-06-09 16:33:22 +0800

    dev: refresh breakpoints

--- web/index.tsx
+++ web/index.tsx
@@ -1,10 +1,12 @@
-// window.addEventListener('unload', function () {
-//   debugger
-// })
+if (location.hostname === 'dev.agnai.chat') {
+  window.addEventListener('unload', function () {
+    debugger
+  })
 
-// window.addEventListener('beforeunload', function () {
-//   debugger
-// })
+  window.addEventListener('beforeunload', function () {
+    debugger
+  })
+}
 
 import { Component } from 'solid-js'
 import { render } from 'solid-js/web'
