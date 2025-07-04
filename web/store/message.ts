85d7a0510074061f931fa04ec45a737aee9a21b2
agnaidev <dev@agnai.chat>
2025-07-04 10:43:55 +0800

    fix image prompt gen

--- web/store/message.ts
+++ web/store/message.ts
@@ -849,10 +849,10 @@ export const msgStore = createStore<MsgState>(
 
       console.log('[wait] gen-img-prompt')
       yield { waiting: undefined }
-      if (res.result) {
-        console.log(`Image Prompt:\n${res.result.response}`)
+      if (res) {
+        console.log(`Image Prompt:\n${res}`)
         // msgStore.editMessageProp(messageId, { imagePrompt: res.result.response })
-        callbacks.onSummary?.(res.result.response)
+        callbacks.onSummary?.(res)
         return
       }
 
