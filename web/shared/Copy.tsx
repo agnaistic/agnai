632d1bf165cb79f46ea4e7b252a3fa3e8820aa9b
agnaidev <dev@agnai.chat>
2025-07-04 11:16:36 +0800

    loading spinner for api key copy

--- web/shared/Copy.tsx
+++ web/shared/Copy.tsx
@@ -1,5 +1,6 @@
 import { ClipboardCheck, ClipboardCopy } from 'lucide-solid'
 import { Component, createSignal, Match, Switch } from 'solid-js'
+import { RelativeSpinner } from './Loading'
 
 /**
  *
@@ -9,15 +10,20 @@ import { Component, createSignal, Match, Switch } from 'solid-js'
 export const Copy: Component<{ text: string; size?: number; onClick?: () => Promise<string> }> = (
   props
 ) => {
+  const [loading, setLoading] = createSignal(false)
   const [clicked, setClicked] = createSignal(false)
 
   const copy = async () => {
     setClicked(true)
 
     try {
+      if (props.onClick) {
+        setLoading(true)
+      }
       const text = props.onClick ? await props.onClick?.() : props.text
       navigator.clipboard.writeText(text)
     } finally {
+      setLoading(false)
       setTimeout(() => setClicked(false), 1000)
     }
   }
@@ -25,6 +31,9 @@ export const Copy: Component<{ text: string; size?: number; onClick?: () => Prom
   return (
     <div onClick={copy} class="icon-button cursor-pointer font-normal">
       <Switch>
+        <Match when={loading()}>
+          <RelativeSpinner size={props.size ?? 20} />
+        </Match>
         <Match when={!clicked()}>
           <ClipboardCopy size={props.size ?? 20} />
         </Match>
